deb4c6c2969f0e0cb97fb556217c6454e2d17cdb
Josh Whitman <jwhitma3@my.westga.edu>
2026-07-20 01:55:32 -0400

    sets up a basic window that will be the main gui for the app

--- main.py
+++ main.py
@@ -3,10 +3,34 @@
 
 """Main entry point for FluxLab."""
 
+import sys
+
+from PySide6.QtCore import QFile, QIODevice
+from PySide6.QtUiTools import QUiLoader
+from PySide6.QtWidgets import QApplication
+
 
 def main() -> None:
     """Run the main application."""
-    pass
+    app = QApplication(sys.argv)
+
+    ui_file_path = "view/ui/mainwindow.ui"
+    ui_file = QFile(ui_file_path)
+    if not ui_file.open(QIODevice.ReadOnly):
+        print(f"Cannot open {ui_file_path}: {ui_file.errorString()}")
+        sys.exit(-1)
+
+    loader = QUiLoader()
+    window = loader.load(ui_file)
+    ui_file.close()
+
+    if not window:
+        print(loader.errorString())
+        sys.exit(-1)
+
+    window.setWindowTitle("Flux Lab")
+    window.show()
+    sys.exit(app.exec())
 
 
 if __name__ == "__main__":
